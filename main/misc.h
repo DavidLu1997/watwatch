48ddeb62a9b8998a4590c5eb77962b288e3294fa
Zopper9000 <david.lu@hotmail.com>
2015-10-15 21:59:31 -0400

    hardcoded date and time elapsed

--- main/misc.h
+++ main/misc.h
@@ -15,14 +15,47 @@ struct date {
   int hour;
   int minute;
   int second;
+  int ms;
 };
 
+//Hard coded begin date
+struct date startDate = {2015, 10, 29, 18, 9, 45, 0};
+
+//Current date
+struct date currentDate = startDate;
+
+//Hard coded day of month
+int month[] = {31, (startDate.year % 4 == 0 && startDate.year % 100 != 0) || startDate.year % 400 == 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30 ,31, 30, 31};
+
+//Get current date based on program runtime
 struct date getDate() {
-  struct date now = {0, 0, 0, 0, 0, 0};
-  now.year = 2015;
-  now.month = 10;
-  now.day = 13;
-  return now;
+  currentDate.ms += millis();
+  currentDate.second += currentDate.ms / 1000;
+  currentDate.ms = currentDate.ms % 1000;
+  currentDate.minute += currentDate.second / 60;
+  currentDate.second = currentDate.second % 60;
+  currentDate.hour += currentDate.minute / 60;
+  currentDate.minute = currentDate.minute % 60;
+  currentDate.day += currentDate.hour / 24;
+  currentDate.hour = currentDate.hour % 24;
+  currentDate.month += currentDate.day / month[currentDate.month - 1];
+  currentDate.day = currentDate.day % month[currentDate.month - 1];
+  currentDate.year += currentDate.month / 12;
+  currentDate.month = currentDate.month % 12;
+  
+  return currentDate;
+}
+
+struct date getTimeElapsed(struct date a, struct date b) {
+  struct date d;
+  d.ms = abs(b.ms - a.ms);
+  d.second = abs(b.second - a.second); 
+  d.minute = abs(b.minute - a.minute);
+  d.hour = abs(b.hour - a.hour);
+  d.day = abs(b.day - a.day);
+  d.month = abs(b.month - a.month);
+  d.year = abs(b.year - a.year);
+  return d;
 }
 
 int secondsSince(struct date a) {
