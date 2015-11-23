060d5bbac1609a9c81b8020d30bcd75a9c5bac84
Ryan Wang <shirui.wang@hotmil.com>
2015-11-23 15:12:26 -0500

    Adjusted text positions

--- new/new.ino
+++ new/new.ino
@@ -20,33 +20,42 @@ void setup()
 
 void loop()
 {
+  
+  //Display, pause, then loop through menu options
+  
+  delay(1000);
   // put your main code here, to run repeatedly:
-  OrbitOledSetCursor(4, 2);
+  OrbitOledSetCursor(6, 1.5);
   OrbitOledPutString("TIME");
   OrbitOledUpdate();
+  delay(1000);
   OrbitOledClear();
   
   delay(1000);
-  OrbitOledSetCursor(4, 2);
+  OrbitOledSetCursor(4, 1);
   OrbitOledPutString("STOPWATCH");
   OrbitOledUpdate();
+  delay(1000);
   OrbitOledClear();
   
   delay(1000);
-  OrbitOledSetCursor(4, 2);
+  OrbitOledSetCursor(6, 1);
   OrbitOledPutString("ALARM");
   OrbitOledUpdate();
+  delay(1000);
   OrbitOledClear();
   
   delay(1000);
-  OrbitOledSetCursor(4, 2);
+  OrbitOledSetCursor(6, 1);
   OrbitOledPutString("STEPS");
   OrbitOledUpdate();
+  delay(1000);
   OrbitOledClear();
   
   delay(1000);
-  OrbitOledSetCursor(4, 2);
+  OrbitOledSetCursor(6, 1);
   OrbitOledPutString("TEMP");
   OrbitOledUpdate();
+  delay(1000);
   OrbitOledClear();
 }
