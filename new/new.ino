518129b2bef6c50184c8ccf63313943c63094fd9
Ryan Wang <shirui.wang@hotmil.com>
2015-11-23 15:44:45 -0500

    Got button working to cycle through menu options

--- new/new.ino
+++ new/new.ino
@@ -1,68 +1,77 @@
-extern "C" {
-#include <delay.h>
-#include <FillPat.h>
-#include <I2CEEPROM.h>
-#include <LaunchPad.h>
-#include <OrbitBoosterPackDefs.h>
-#include <OrbitOled.h>
-#include <OrbitOledChar.h>
-#include <OrbitOledGrph.h>
-}
-
-
-void setup()
-{
+  extern "C" {
+  #include <delay.h>
+  #include <FillPat.h>
+  #include <I2CEEPROM.h>
+  #include <LaunchPad.h>
+  #include <OrbitBoosterPackDefs.h>
+  #include <OrbitOled.h>
+  #include <OrbitOledChar.h>
+  #include <OrbitOledGrph.h>
+  }
   
-  // put your setup code here, to run once:
-  GPIOPinTypeGPIOInput(BTN1Port, BTN1);
-  GPIOPinTypeGPIOInput(BTN2Port, BTN2);
-  lBtn1 = GPIOPinRead(BTN1Port, BTN1);
-  OrbitOledInit();
-  OrbitOledClear();
-}
-
-void loop()
-{
-  long 	lBtn1;
-  //Display, pause, then loop through menu options
+  //Variables
+  int i = 0;
+  long 	lBtn2;
   
-  delay(1000);
-  // put your main code here, to run repeatedly:
-  OrbitOledSetCursor(6, 1.5);
-  OrbitOledPutString("TIME");
-  OrbitOledUpdate();
-  delay(1000);
-  OrbitOledClear();
   
-  if(lBtn1 == BTN1) {
+  void setup()
+  {
+    
+    // put your setup code here, to run once:
+    
+    OrbitOledInit();
     OrbitOledClear();
   }
   
-  delay(1000);
-  OrbitOledSetCursor(4, 1);
-  OrbitOledPutString("STOPWATCH");
-  OrbitOledUpdate();
-  delay(1000);
-  OrbitOledClear();
-  
-  delay(1000);
-  OrbitOledSetCursor(6, 1);
-  OrbitOledPutString("ALARM");
-  OrbitOledUpdate();
-  delay(1000);
-  OrbitOledClear();
-  
-  delay(1000);
-  OrbitOledSetCursor(6, 1);
-  OrbitOledPutString("STEPS");
-  OrbitOledUpdate();
-  delay(1000);
-  OrbitOledClear();
-  
-  delay(1000);
-  OrbitOledSetCursor(6, 1);
-  OrbitOledPutString("TEMP");
-  OrbitOledUpdate();
-  delay(1000);
-  OrbitOledClear();
-}
+  void loop()
+  {
+    //Bottom button
+    GPIOPinTypeGPIOInput(BTN1Port, BTN1);
+    
+    //Top Button
+    GPIOPinTypeGPIOInput(BTN2Port, BTN2);
+    lBtn2 = GPIOPinRead(BTN2Port, BTN2);
+    
+    //Display menu option based on current value of i, which is adjusted when button is clicked
+    if (i == 0) {
+      OrbitOledSetCursor(6, 1.5);
+      OrbitOledPutString("TIME");
+      OrbitOledUpdate();
+    }
+    else if (i==1) {
+        OrbitOledSetCursor(4, 1);
+        OrbitOledPutString("STOPWATCH");
+        OrbitOledUpdate();
+    }
+    else if (i==2) {
+        OrbitOledSetCursor(5, 1);
+        OrbitOledPutString("ALARM");
+        OrbitOledUpdate();
+    }
+    else if (i==3) {
+         OrbitOledSetCursor(6, 1);
+         OrbitOledPutString("STEPS");
+         OrbitOledUpdate();
+    }
+    else if (i==4) {
+       OrbitOledSetCursor(6, 1);
+       OrbitOledPutString("TEMP");
+       OrbitOledUpdate();
+    }
+    
+    
+    if(lBtn2 == BTN2) {
+      //Increase i when button is clicked
+      if (i < 4) {
+         i++;
+         OrbitOledClear();
+         delay(200);
+      }
+      //Reset to 0 so that it'll loop through the options
+      else if (i == 4) {
+        i = 0;
+        OrbitOledClear();
+        delay(200);
+      }
+    }
+  }
