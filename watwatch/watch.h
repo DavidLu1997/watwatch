1a28f626c73c4ac3a1cd916ec9d3b9c6ea254aa2
Zopper9000 <david.lu@hotmail.com>
2015-11-25 22:09:58 -0500

    fixed watch stuff

--- watwatch/watch.h
+++ watwatch/watch.h
@@ -12,6 +12,24 @@
 #define TIMER_DELAY 1000
 #define STOPWATCH_DELAY 1
 
+//Declarations
+void initWatch();
+void drawWatch();
+void updateTime();
+void updateTimer();
+void getWatch();
+void timer(int s);
+void checkAlarm();
+void setAlarm(struct date d);
+void startStopWatch();
+void pauseStopWatch();
+void stopStopWatch();
+void resetStopWatch();
+int getStopWatch();
+void drawTimer();
+void drawAlarm();
+void drawStopWatch();
+
 //Current date
 struct date current;
 
@@ -39,6 +57,26 @@ void initWatch() {
 //Draws current function, continuously called
 //DRAW_DELAY
 void drawWatch() {
+
+}
+
+//Checks and updates current time, continuously called
+void updateTime() {
+	current = getDate();
+}
+
+//Draw timer
+void drawTimer() {
+
+}
+
+//Draw alarm
+void drawAlarm() {
+
+}
+
+//Draw stopWatch
+void drawStopWatch() {
 
 }
 
