46277bd2e9a3fc8c10529b799c287baa9e1a347c
Mara Gagiu <mara.gagiu@gmail.com>
2015-11-25 22:20:02 -0500

    Added Default Timer

--- watwatch/watch.h
+++ watwatch/watch.h
@@ -25,6 +25,15 @@ int alarmSize = 0;
 int timers[MAX_TIMERS];
 int timerSize = 0;
 
+
+//Timer Data
+int s_Timer=0;
+int m_Timer=0;
+int h_Timer=0;
+
+char secDisplay[2];
+char minDisplay[2];
+char hourDisplay[2];
 //Stopwatch
 int stopwatch = 0;
 bool stopwatchRunning = false;
@@ -39,7 +48,30 @@ void initWatch() {
 //Draws current function, continuously called
 //DRAW_DELAY
 void drawWatch() {
+        //Display Title
+        OrbitOledSetCursor(4 , 1);
+        OrbitOledPutString("Title: ");
 
+        //Displaying steps number
+        itoa(h_Timer, hourDisplay, 10);
+        OrbitOledSetCursor(7, 1);
+        OrbitOledPutString(hourDisplay);
+        
+        OrbitOledSetCursor(11,1);
+        OrbitOledPutString(":");
+        
+        itoa(m_Timer, minDisplay, 10);
+        OrbitOledSetCursor(13, 1);
+        OrbitOledPutString(minDisplay);
+       
+        OrbitOledSetCursor(15,1);
+        OrbitOledPutString(":");
+        
+        itoa(s_Timer,secDisplay,10);
+        OrbitOledSetCursor(15,1);
+        OrbitOledPutString(secDisplay); 
+      
+  
 }
 
 //Checks and updates current time, continuously called
